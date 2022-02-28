9ac454bec79d2f33b9f5d73f76cbb344c4e94393
Ubuntu <ubuntu@ip-172-31-15-252.ec2.internal>
2022-02-28 14:19:55 +0000

    fhkfhd

---
+++ areaPerRect.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2021 Daniel Imperadeiro All Rights Reserved
+//
+// Created By: Daniel
+// Date: Feb. 22, 2022
+// This program displays the area and perimeter of a rectangle
+
+#include <iostream>
+
+int main() {
+  std::cout << "For a rectangle that has the dimensions\n";
+  std::cout << "length 5 cm and width 7 cm:\n";
+  std::cout << "\narea = " << (5 * 7) << " cm^2";
+  std::cout << "\nperimeter = " << (2 * (5 + 7)) << " cm";
+}
