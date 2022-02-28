f603769ca83c764da092a0087d01809623c0f2a3
Ubuntu <ubuntu@ip-172-31-15-252.ec2.internal>
2022-02-28 14:12:26 +0000

    jjsdfhsd

---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Daniel Imperadeiro All Rights Reserved
+//
+// Created By:Daniel
+// Date: Feb. 22, 2022
+// This program displays some basic math
+
+#include <iostream>
+
+int main() {
+  std::cout << "\n25 / 3 = " << (25 / 3);
+  std::cout << "\n37 + 24 = " << (37 + 24);
+  std::cout << "\n17 - 6 = " << (17 - 6);
+  std::cout << "\n5 x 13 = " << (5 * 13);
+  std::cout << "\n27 - (24 / 8) = " << (27 - (24 / 8));
+  std::cout << "\n25 % 7 = " << (25 % 7);
+}
